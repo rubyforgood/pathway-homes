d48c8084ea219cca4e9a4741fb4195691fa5c0be
Britney Wright <blw06g@gmail.com>
2014-08-01 17:21:44 -0400

    index data and layout

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'service_requests#new'
 
-  resources :service_requests, only: [:new, :index]
+  resources :service_requests, only: [:new, :index, :show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
