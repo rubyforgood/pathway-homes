d2ae90604b1e99a81b4e4c8f6a79966b9d7e66ee
Brandon Rice <brice84@gmail.com>
2014-08-01 13:41:12 -0400

    Add ServiceRequests controller for backend and frontend.

--- app/assets/javascripts/pathway_homes.js
+++ app/assets/javascripts/pathway_homes.js
@@ -1,5 +1,2 @@
 var pathwayHomes = angular.module("pathwayHomes", []);
 
-pathwayHomes.controller("HomeCtlr", ["$scope", function($scope) {
-  $scope.testing = "Hello World";
-}]);
---
+++ app/controllers/service_requests_controller.rb
@@ -0,0 +1,4 @@
+class ServiceRequestsController < ApplicationController
+  def new
+  end
+end
