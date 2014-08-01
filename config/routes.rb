820de0512ac5380830bab537dd6cd37fb77de1e1
mepps <maggie.epps@gmail.com>
2014-08-01 18:22:40 -0400

    changed registration route to user/add_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  devise_scope :user do
+    get '/users/add_user' => 'devise/registrations#new'
+  end
+  
   resources :service_requests
   root to: 'service_requests#new'
 end
