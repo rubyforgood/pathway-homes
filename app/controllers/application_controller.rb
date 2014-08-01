ad70b644a20f716bf567bffa66206380944f1999
Andy Lindeman <andy@lindeman.io>
2014-08-01 18:13:43 -0400

    User must reset their password if it is expired

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,19 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :check_for_expired_password, if: ->{ user_signed_in? && !devise_controller? }
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:sign_up) << :role
+  end
+
+  def check_for_expired_password
+    if current_user.password_expired?
+      flash[:alert] = "Your password has expired. Please reset it before continuing."
+      redirect_to edit_user_registration_path
+    end
   end
 end
