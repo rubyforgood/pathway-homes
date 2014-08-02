9d7425cccf6c5e1fa16869553f97082c383aecc6
Andy Lindeman <andy@lindeman.io>
2014-08-02 16:58:56 -0400

    Adds EmailDigest.deliver_latest

---
+++ app/models/email_digest_delivery.rb
@@ -0,0 +1,22 @@
+class EmailDigestDelivery < ActiveRecord::Base
+  DELIVERY_FREQUENCY = 1.day
+
+  def self.deliver_latest
+    last_digest      = self.order(:delivered_at).last
+    last_digest_time = (last_digest && last_digest.delivered_at) || Time.at(0)
+    now              = Time.now
+
+    if (last_digest_time + DELIVERY_FREQUENCY) <= now
+      recipients       = User.admin.map(&:email)
+      service_requests = ServiceRequest.where(created_at: last_digest_time...now)
+
+      EmailDigest.digest(
+        recipients: recipients,
+        last_digest_time: last_digest_time,
+        service_requests: service_requests,
+      ).deliver
+
+      new(delivered_at: now).save!
+    end
+  end
+end
