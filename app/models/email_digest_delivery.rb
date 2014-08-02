fc82f9ff11bd4689cea210fcf34e037e9036fecf
Andy Lindeman <andy@lindeman.io>
2014-08-02 18:37:51 -0400

    Modifies delivery logic for #67
    Closes #67

--- app/models/email_digest_delivery.rb
+++ app/models/email_digest_delivery.rb
@@ -1,14 +1,22 @@
 class EmailDigestDelivery < ActiveRecord::Base
-  DELIVERY_FREQUENCY = 1.day
+  TIME_ZONE       = "Eastern Time (US & Canada)"
+  DELIVERY_HOUR   = 15
+  DELIVERY_MINUTE = 30
 
   def self.deliver_latest
     last_digest      = self.order(:delivered_at).last
-    last_digest_time = (last_digest && last_digest.delivered_at) || Time.at(0)
-    now              = Time.now
+    last_digest_time = ((last_digest && last_digest.delivered_at) || Time.at(0)).in_time_zone(TIME_ZONE)
+    last_digest_date = last_digest_time.to_date
 
-    if (last_digest_time + DELIVERY_FREQUENCY) <= now
+    now              = Time.now.in_time_zone(TIME_ZONE)
+    today            = now.to_date
+    delivery_time    = now.change(hour: DELIVERY_HOUR, min: DELIVERY_MINUTE)
+
+    # if the email hasn't been delivered 'today' and it's past the
+    # [DELIVERY_HOUR, DELIVERY_MINUTE], send the email
+    if today > last_digest_date && now >= delivery_time
       recipients       = User.admin.map(&:email)
-      service_requests = ServiceRequest.where(created_at: last_digest_time...now).order_by(:created_at)
+      service_requests = ServiceRequest.where(created_at: last_digest_time...now).order(:created_at)
 
       EmailDigest.digest(
         recipients: recipients,
