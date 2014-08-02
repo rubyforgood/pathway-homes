e334c520c5abc48155fe465c6f79a35fca220af3
madtypist <mad_typist@yahoo.com>
2014-08-02 11:21:56 -0400

    Create some static pages. Update routes file to point to them. Put some initial fancy styles in to see what people think.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
   devise_for :users
   devise_scope :user do
     get '/users/add_user' => 'devise/registrations#new'
   end
   
   resources :service_requests
-  root to: 'service_requests#new'
+  root to: 'pages#index'
 end
