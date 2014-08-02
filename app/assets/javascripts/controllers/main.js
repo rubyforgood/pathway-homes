1afab4b37d366ddcf28e381ec1848be7a78de826
Brandon Rice <brice84@gmail.com>
2014-08-02 11:08:48 -0400

    Manage competing alerts in a promise.

--- app/assets/javascripts/controllers/main.js
+++ app/assets/javascripts/controllers/main.js
@@ -1,5 +1,5 @@
 pathwayHomes.controller("MainCtlr", ["$scope", "$timeout", function($scope, $timeout) {
-  $scope.notice = {type: "alert-info", message: ""};
+  $scope.notice = {promise: null, type: "alert-info", message: ""};
 
   $scope.alert = function(message, type) {
     $scope.notice.type = "alert-" + type;
@@ -8,7 +8,8 @@ pathwayHomes.controller("MainCtlr", ["$scope", "$timeout", function($scope, $tim
   };
 
   $scope.$watch("notice.message", function(newValue) {
-    $timeout(function() { $scope.notice.message = ""; }, 5000);
+    $timeout.cancel($scope.notice.promise);
+    $scope.notice.promise = $timeout(function() { $scope.notice.message = ""; }, 5000);
   });
 
   $scope.notice.message = $('.alert').data('alert');
