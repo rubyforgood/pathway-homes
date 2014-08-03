3a65319772b7fa25ca0f185c39f809e358897d38
Ryan Malecky <rmalecky@gmail.com>
2014-08-03 16:56:11 -0400

    adds export front end

--- app/assets/javascripts/pathway_homes.js
+++ app/assets/javascripts/pathway_homes.js
@@ -1,5 +1,6 @@
 var pathwayHomes = angular.module("pathwayHomes", ['ngRoute', "localytics.directives"]);
 
+
 //pathwayHomes.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
 //  $routeProvider.
 //    when("/service_requests/new", {controller: 'ServiceRequestsCtlr'});
