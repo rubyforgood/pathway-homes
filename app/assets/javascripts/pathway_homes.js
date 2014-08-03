5af06824a81727e6bba6a0398eba2a3633cf4ccb
Andy Lindeman <andy@lindeman.io>
2014-08-03 11:20:24 -0400

    In the interest of time, avoids parsing to a `Date` in angular

--- app/assets/javascripts/pathway_homes.js
+++ app/assets/javascripts/pathway_homes.js
@@ -1,33 +1,5 @@
 var pathwayHomes = angular.module("pathwayHomes", ['ngRoute', "localytics.directives"]);
 
-pathwayHomes.directive('dateField', function() {
-  return {
-    require: 'ngModel',
-    link: function (scope, elem, attrs, ngModel) {
-      var toView = function(val) {
-        console.log(val);
-        if (val === undefined) {
-          return "";
-        } else {
-          return val.getFullYear() + "-" + (val.getMonth() + 1) + "-" + val.getDate();
-        }
-      };
-
-      var toModel = function(val) {
-        console.log(val);
-        if (val === undefined) {
-          return "";
-        } else {
-          return new Date(val);
-        }
-      };
-
-      ngModel.$formatters.unshift(toView);
-      ngModel.$parsers.unshift(toModel);
-    }
-  };
-});
-
 //pathwayHomes.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
 //  $routeProvider.
 //    when("/service_requests/new", {controller: 'ServiceRequestsCtlr'});
